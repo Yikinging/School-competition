4c8ef919631894fff7498d477fd023115604cb5c
boxing <boxs@boxing.com>
2026-04-13 18:27:05 +0800

    添加PID错误管理，删除不必要的占空比限幅

--- User/Midware/Inc/Buck.h
+++ User/Midware/Inc/Buck.h
@@ -16,13 +16,25 @@ typedef struct {
 } Buck_InitTypeDef;
 
 typedef enum{
-    BUCK_INVALID = 0U,
+    BUCK_ERROR = 0U,
     BUCK_DISABLED,
     BUCK_CVMODE,
     BUCK_CCMODE
 }Buck_StateTypeDef;
 
+typedef enum{
+    BUCK_NOERROR = 0U,
+    BUCK_OCP = 1U << 0,
+    BUCK_OVP = 1U << 1
+}Buck_FaultFlagTypeDef;
+
 typedef struct Buck_HandleTypeDef Buck_HandleTypeDef;
+
+#define BUCK_OCTHRESHOLD 0.5f
+#define BUCK_OVTHRESHOLD 10.0f
+
+#define BUCK_MAX_DUTY 0.90f
+#define BUCK_MIN_DUTY 0.10f
 
 Buck_HandleTypeDef* Buck_Init(Buck_InitTypeDef* init);
 void Buck_DeInit(Buck_HandleTypeDef* handle);
@@ -30,6 +42,8 @@ void Buck_DeInit(Buck_HandleTypeDef* handle);
 void Buck_Start(Buck_HandleTypeDef* handle);
 void Buck_Stop(Buck_HandleTypeDef* handle);
 Buck_StateTypeDef Buck_GetState(Buck_HandleTypeDef* handle);
+Buck_FaultFlagTypeDef Buck_GetFaultFlag(Buck_HandleTypeDef* handle);
+void Buck_ClearFaultFlag(Buck_HandleTypeDef* handle);
 
 void Buck_SetValue(Buck_HandleTypeDef* handle, float Voltage, float Current);
 float Buck_GetDuty(Buck_HandleTypeDef* handle);
