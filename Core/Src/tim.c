7fba5505de96b0ff5dd6dde897b36f7d4c17c0b8
boxing <boxs@boxing.com>
2026-04-14 16:37:11 +0800

    重定义ADC的触发源，ADC1循环模式软件触发 TIM1TRGO回复为RESET

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -58,7 +58,7 @@ void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_OC4REF;
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
   {
