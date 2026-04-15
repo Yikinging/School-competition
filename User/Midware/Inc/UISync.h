91e271671f85be05a3f17092eecff3b6ea0598e2
boxing <boxs@boxing.com>
2026-04-15 19:14:08 +0800

    UISync模块更新

--- User/Midware/Inc/UISync.h
+++ User/Midware/Inc/UISync.h
@@ -1,21 +1,34 @@
 #ifndef __UISYNC_H__
 #define __UISYNC_H__
 
-void UISync_Init(void);
-void UISync_DeInit(void);
+#include <stdint.h>
+
+// void UISync_Init(void);
+// void UISync_DeInit(void);
+
+typedef struct UISync_DeviceHandleTypedef UISync_DeviceHandleTypedef;
 
 typedef enum{
-    UISync_Error = 0U,
+    UISync_Undefined = 0U,
     UISync_Pending,
-    UISync_Waiting
+    UISync_Waiting,
+    UISync_Disabled
 } UISync_DeviceStatusTypedef;
 
 typedef struct{
     void (*Init)(void);
     void (*DeInit)(void);
-    UISync_DeviceStatusTypedef (*GetStatus)(void);
+    uint8_t Rank;
+    void* DataSheet;
+    UISync_DeviceStatusTypedef (*Update)(void);
     void (*Process)(void);
 } UISync_DeviceInitTypedef;
+
+#define UISYNC_MaxDeviceNum 10U
+
+UISync_DeviceHandleTypedef* UISync_RegisterDevice(const UISync_DeviceInitTypedef *init);
+void UISync_UnregisterDevice(UISync_DeviceHandleTypedef *handle);
+void UISync_SetDeviceStatus(UISync_DeviceHandleTypedef *handle, UISync_DeviceStatusTypedef status);
 
 void UISync_Update(void);
 void UISync_Process(void);
--- User/Midware/Src/UISync.c
+++ User/Midware/Src/UISync.c
@@ -1,2 +1,80 @@
-#include ""UISync.h"
+#include "UISync.h"
+#include <stddef.h>
 
+struct UISync_DeviceHandleTypedef {
+    UISync_DeviceInitTypedef Init;
+    UISync_DeviceStatusTypedef Status;
+};
+
+static UISync_DeviceHandleTypedef UISync_DeviceList[UISYNC_MaxDeviceNum] = {0};
+
+UISync_DeviceHandleTypedef* UISync_RegisterDevice(const UISync_DeviceInitTypedef* init){
+    if(init == NULL
+        || init->Init == NULL
+        || init->DeInit == NULL
+        || init->Update == NULL
+        || init->Process == NULL){
+        return NULL;
+    }
+
+    if (init->Rank >= UISYNC_MaxDeviceNum){
+        return NULL;
+    }
+    if (UISync_DeviceList[init->Rank].Status != UISync_Undefined){
+        return NULL;
+    }
+
+    UISync_DeviceHandleTypedef* handle = &UISync_DeviceList[init->Rank];
+    handle->Init = *init;
+    handle->Status = UISync_Waiting;
+
+    init->Init();
+
+    return handle;
+}
+
+void UISync_UnregisterDevice(UISync_DeviceHandleTypedef *handle){
+    if(handle == NULL
+        || handle->Init.DeInit == NULL){
+        return;
+    }
+
+    handle->Init.DeInit();
+
+    (*handle) = (UISync_DeviceHandleTypedef){0};
+}
+
+void UISync_SetDeviceStatus(UISync_DeviceHandleTypedef *handle, UISync_DeviceStatusTypedef status){
+    if(handle == NULL){
+        return;
+    }
+    handle->Status = status;
+}
+
+// placed at interrupt function
+void UISync_Update(void){
+    for(int i = 0; i < UISYNC_MaxDeviceNum; i++){
+        UISync_DeviceHandleTypedef* handle = &UISync_DeviceList[i];
+        if (handle->Status == UISync_Undefined){
+            continue;
+        }
+        if (handle->Status == UISync_Disabled){
+            continue;
+        }
+        handle->Status = handle->Init.Update();
+    }
+}
+
+// placed at main loop
+void UISync_Process(void){
+    for(int i = 0; i < UISYNC_MaxDeviceNum; i++){
+        UISync_DeviceHandleTypedef *handle = &UISync_DeviceList[i];
+        if (handle->Status == UISync_Undefined){
+            continue;
+        }
+        if (handle->Status == UISync_Pending){
+            handle->Status = UISync_Waiting;
+            handle->Init.Process();
+        }
+    }
+}
